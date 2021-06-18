9fd160f8050c3c65bcf29975b6c80dd4cd5a7e10
Jasper <jasperwieringa@gmail.com>
2021-06-19 01:45:55 +0200

    Added the department selection from the AppUserDepartment enumeration

--- src/main/java/com/main/easyFix/appuser/AppUserDepartment.java
+++ src/main/java/com/main/easyFix/appuser/AppUserDepartment.java
@@ -1,8 +1,15 @@
 package com.main.easyFix.appuser;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum AppUserDepartment {
-  ADMIN,
-  EXPERT,
-  CASHIER,
-  BACKOFFICE
+  ADMIN("Administration"),
+  EXPERT("Expert"),
+  CASHIER("Cashier"),
+  BACKOFFICE("Backoffice");
+
+  private final String displayValue;
 }
