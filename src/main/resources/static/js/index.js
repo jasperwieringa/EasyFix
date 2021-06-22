85561abe2c6cb4a73110f8e8027c89de22856518
Jasper <jasperwieringa@gmail.com>
2021-06-22 13:49:46 +0200

    Replaced the form alerts with a customer Bootstrap form validation so the alerts are shown in the Modal instead of the landing pages

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,10 +1,26 @@
 $(function () {
   // Bootstrap tooltips
-  $('[data-toggle="tooltip"]').tooltip()
+  $("[data-toggle='tooltip']").tooltip()
 
   // Clear the dialogs on open
   $(".modal").on("shown.bs.modal", () => {
     $("input").val("");
   })
+
+  // Bootstrap form validation
+  window.addEventListener("load", function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.getElementsByClassName("needs-validation");
+    // Loop over them and prevent submission
+    const validation = Array.prototype.filter.call(forms, form => {
+      form.addEventListener("submit", event => {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add("was-validated");
+      }, false);
+    });
+  }, false);
 })
 
