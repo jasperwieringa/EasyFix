5ad350eb16ddab8a1f3c37d58e07ad3ea4d958f0
Jasper <jasperwieringa@gmail.com>
2021-06-25 00:46:24 +0200

    Finished setting up the PostgreSQL database and the required properties

--- src/main/java/com/main/easyFix/EasyFixApplication.java
+++ src/main/java/com/main/easyFix/EasyFixApplication.java
@@ -1,27 +1,59 @@
 package com.main.easyFix;
 
+import com.main.easyFix.appuser.AppUser;
+import com.main.easyFix.appuser.AppUserRepository;
+import com.main.easyFix.customer.Customer;
+import com.main.easyFix.customer.CustomerRepository;
+import com.main.easyFix.parts.Part;
+import com.main.easyFix.parts.PartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+import java.util.List;
 
 @SpringBootApplication
-public class EasyFixApplication extends SpringBootServletInitializer implements CommandLineRunner {
+public class EasyFixApplication extends SpringBootServletInitializer {
 	@Autowired
-	private JdbcTemplate jdbcTemplate;
+	private DataSource dataSource;
+	@Autowired
+	private AppUserRepository appUserRepository;
+	@Autowired
+	private CustomerRepository customerRepository;
+	@Autowired
+	private PartRepository partRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EasyFixApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		String sql = "INSERT INTO USERS ( APP_USER_DEPARTMENT, EMAIL, ENABLED, FIRST_NAME, LAST_NAME, LOCKED, PASSWORD ) VALUES (" +
-			"'ADMIN', 'admin@easyfix.nl', TRUE, 'Admin', 'EasyFix', FALSE, '$2y$12$zgwFyki4zI0WNZl9r9BZZeKIF7DbKoDLI46xJEg6zinwSiyHoyfLy')";
+	@EventListener(ApplicationReadyEvent.class)
+	//	TODO: Remove, The loadData method is only used to add some test data for the assignment.
+	public void loadData() {
+		System.out.println("Pre-populating the database with test data");
 
-		jdbcTemplate.update(sql);
+		ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator(
+			false,
+			false,
+			"UTF-8",
+			new ClassPathResource("data.sql"));
+		resourceDatabasePopulator.execute(dataSource);
+
+		List<AppUser> allUsers = appUserRepository.findAll();
+		List<Customer> allCustomers = customerRepository.findAll();
+		List<Part> allPArts = partRepository.findAll();
+
+		if (allUsers.size() != 0 && allCustomers.size() != 0 && allPArts.size() != 0) {
+			System.out.println("Database successfully pre-populated with test data!");
+		} else {
+			System.out.println("Could not pre-populate database with test data!");
+		}
 		System.out.println("Application successfully started!");
 	}
 }
